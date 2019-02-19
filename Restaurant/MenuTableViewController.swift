4397245dd9829632e71c0e69f1cdcf21068821b5
Seng Hwwa <senghwwa@zana-girl.local>
2019-02-19 15:39:28 +0800

    Categories in place. To code menu items

--- Restaurant/MenuTableViewController.swift
+++ Restaurant/MenuTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MenuTableViewController: UITableViewController {
 
+	var category: String!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -77,14 +79,17 @@ class MenuTableViewController: UITableViewController {
     }
     */
 
-    /*
+	
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+	/*
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == "identifier" {
+	
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+	*/
 
 }
