196e1bfeab463e603312421fcf0a29805ba786b9
Seng Hwwa <senghwwa@zana-girl.local>
2019-02-21 15:50:44 +0800

    Image retrieval to be coded

---
+++ Restaurant/OrderConfirmationViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OrderConfirmationViewController.swift
+//  Restaurant
+//
+//  Created by Seng Hwwa on 21/02/2019.
+//  Copyright © 2019 Seng Hwwa. All rights reserved.
+//
+
+import UIKit
+
+class OrderConfirmationViewController: UIViewController {
+
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    var minutes: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        timeRemainingLabel.text = "Thank you for your order! Your wait time is approximately \(minutes!) minutes"
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Restaurant/OrderTableViewController.swift
+++ Restaurant/OrderTableViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 //var order = Order()
 
+
 class OrderTableViewController: UITableViewController {
 
+    var orderMinutes = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 		
@@ -77,14 +80,54 @@ class OrderTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ConfirmationSegue" {
+            let orderConfirmationViewController = segue.destination as! OrderConfirmationViewController
+            orderConfirmationViewController.minutes = orderMinutes
+        }
     }
-    */
+    
+
+	@IBAction func unwindToOrderList(segue: UIStoryboardSegue) {
+        if segue.identifier == "DismissConfirmation" {
+            MenuController.shared.order.menuItems.removeAll()
+        }
+		
+	}
+	
+    @IBAction func submitTapped(_ sender: UIBarButtonItem) {
+        let orderTotal = MenuController.shared.order.menuItems.reduce(0.0)
+        { (result, menuItem) -> Double in
+            return result + menuItem.price
+        }
+        let formattedOrder = String(format: "$%.2f", orderTotal)
+        
+        let alert = UIAlertController(title: "Confirm Order", message: "You are about to submit your order with a total of \(formattedOrder)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Submit", style: .default) { action in self.uploadOrder()
+        })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+        }
+    
+    func uploadOrder() {
+        let menuIds = MenuController.shared.order.menuItems.map { $0.id }
+        MenuController.shared.submitOrder(forMenuIds: menuIds)
+        { (minutes) in
+            DispatchQueue.main.async {
+                if let minutes = minutes {
+                    self.orderMinutes = minutes
+                    self.performSegue(withIdentifier: "ConfirmationSegue", sender: nil)
+                }
+            }
+            
+        }
+    }
 
 }
+
+
+
