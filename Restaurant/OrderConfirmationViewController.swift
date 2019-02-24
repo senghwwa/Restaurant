fb041a9f15d89c6a7c45e19448bf7d207f80479c
Seng Hwwa <senghwwa@zana-girl.local>
2019-02-24 20:06:33 +0800

    Add code for notification. Commented out at the moment

--- Restaurant/OrderConfirmationViewController.swift
+++ Restaurant/OrderConfirmationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class OrderConfirmationViewController: UIViewController {
 
@@ -17,7 +18,25 @@ class OrderConfirmationViewController: UIViewController {
         super.viewDidLoad()
 
         timeRemainingLabel.text = "Thank you for your order! Your wait time is approximately \(minutes!) minutes"
-        
+		
+		/*
+		let notificationCenter = UNUserNotificationCenter.current()
+		// DO NOT DO THIS notificationCenter.removeAllPendingNotificationRequests()
+		
+		let content = UNMutableNotificationContent()
+		content.title = "Order Due For Collection"
+		let notficationDelay = minutes - 10
+		content.body = "Your order is due in /(notificationDelay) minutes"
+		if notificationDelay <= 0 {
+			notificationDelay = 0
+			content.body = "Your order is due in less than 10 minutes"
+		}
+		let trigger = UNTimeIntervalNotificationTrigger(timeInterval: (notificationDelay * 60), repeats: false)
+		let identifier = "PushNotifierLocalNotification"
+		let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+		notificationCenter.add(request, withCompletionHandler: nil)
+		*/
+		
     }
     
 
